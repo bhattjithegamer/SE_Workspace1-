e557c598d6b4ded102343e359b806bc3b1c32cbf
bhattjithegamer <sbha6482@gmail.com>
2025-05-20 09:21:56 +0530

    commit

---
+++ userinput.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main ()
+{
+    int a;
+
+    printf("enter your number:");
+    scanf("%d",&a);
+
+    printf("you have entered this number: %d",a);
+}
