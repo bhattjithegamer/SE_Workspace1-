0a32117197f5d5727678d4cb67071e87705e3529
bhattjithegamer <sbha6482@gmail.com>
2025-05-25 10:15:02 +0530

    commit

---
+++ 24-5-2025/p3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i,j;
+   
+    for(i=0;i<=5;i++)
+    {
+        for(j=0;j<=i;j++)
+        {
+            char a = 'A'+i;
+            printf("%c",a);
+        }
+        printf("\n");
+    }
+}
