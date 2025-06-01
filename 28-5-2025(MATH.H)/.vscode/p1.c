db189711a6dd284fdc66861581005fdde65d4f91
bhattjithegamer <sbha6482@gmail.com>
2025-06-01 21:43:14 +0530

    commit

---
+++ 28-5-2025(MATH.H)/.vscode/p1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+   
+
+    printf("\n %f",ceil(1.56)); 
+    printf("\n %f",floor(1.56));
+    printf("\n %f",sqrt(121));
+    printf("\n %f",pow(2,3));
+    printf("\n %d",abs(-12));
+    return 0;
+}
